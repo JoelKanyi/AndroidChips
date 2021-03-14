fa7f9d31ef21256ab050ff00980fbd01d1f9e609
JoelKanyi <joelkanyi98@gmail.com>
2021-03-14 10:40:49 +0300

    listen to the selected chip--> single selection mode

--- app/src/main/java/com/kanyideveloper/androidchips/MainActivity.kt
+++ app/src/main/java/com/kanyideveloper/androidchips/MainActivity.kt
@@ -2,10 +2,29 @@ package com.kanyideveloper.androidchips
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import com.kanyideveloper.androidchips.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+   private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.chipGroup.setOnCheckedChangeListener { chipGroup, i ->
+            getSelectedText(chipGroup, i)
+            Toast.makeText(this,getSelectedText(chipGroup, i), Toast.LENGTH_SHORT ).show()
+        }
+
+
+    }
+    private fun getSelectedText(chipGroup: ChipGroup, id: Int): String {
+        val mySelection = chipGroup.findViewById<Chip>(id)
+        return mySelection?.text?.toString() ?: ""
     }
 }
